8c2ee4779cc1451d110a89778c9d96ef61c048c2
SeamusMcFarland <60547736+SeamusMcFarland@users.noreply.github.com>
2020-02-01 13:59:48 -0600

    de-inverted player

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -68,7 +68,7 @@ public class PlayerScript : MonoBehaviour
     {
         directionY += horizontalSpeed * Input.GetAxis("Mouse X");
         directionX += verticalSpeed * Input.GetAxis("Mouse Y");
-        transform.rotation = Quaternion.Euler(directionX, -directionY, 0);
+        transform.rotation = Quaternion.Euler(-directionX, directionY, 0);
     }
 
     private void ShootGlue()
