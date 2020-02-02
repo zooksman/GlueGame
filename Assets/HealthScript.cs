7f7ddddc62ac98b80b807d0ca0af1cde0dd4c769
SeamusMcFarland <60547736+SeamusMcFarland@users.noreply.github.com>
2020-02-02 12:02:58 -0600

    help

---
+++ Assets/HealthScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public GameObject Health;
+    public Text health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HealthChange()
+    {
+        //Who goes to dinner with a compiler error??
+        health = Health.GetComponent<UnityEngine.UI.Text>(); 
+        //changeHealth.text.GetComponent<Text>().text = "health";
+    }
+}
