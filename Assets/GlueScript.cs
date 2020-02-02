ce0f51167f5614efa48a8a28b5855f854d90a18d
SeamusMcFarland <60547736+SeamusMcFarland@users.noreply.github.com>
2020-02-01 18:37:46 -0600

    Wonky glue movement fixed

--- Assets/GlueScript.cs
+++ Assets/GlueScript.cs
@@ -9,10 +9,14 @@ public class GlueScript : MonoBehaviour
     float size;
     bool beingHeld;
     public GameObject hit;
+    bool glued;
+    const float SPEED_DEGREDATION = 0.999f;
+    Vector3 savedVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
+        glued = false;
         beingHeld = false;
         transform.position = new Vector3(300f,300f,300f);
         rb = GetComponent<Rigidbody>();
@@ -22,7 +26,8 @@ public class GlueScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		
+        if (glued)
+            rb.velocity = new Vector3(rb.velocity.x * SPEED_DEGREDATION, rb.velocity.y * SPEED_DEGREDATION, rb.velocity.z * SPEED_DEGREDATION);
     }
 
     public void SetSize(float s)
@@ -33,18 +38,34 @@ public class GlueScript : MonoBehaviour
 
     public void ShootSelf(Vector3 direction)
     {
+        glued = false;
         rb.velocity = new Vector3(0,0,0);
         rb.AddForce(direction * VELOCITY_MODIFIER);
-    }
-    
-    public void OnCollisionEnter(Collision c) {
-    	hit = c.collider.gameObject;
-    	if (hit.CompareTag("shippiece") && hit.GetComponent<ShipPieceScript>().inPlace == false) {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
-    		hit.GetComponent<Rigidbody>().isKinematic = true;
-    		hit.GetComponent<Rigidbody>().transform.parent = GetComponent<Rigidbody>().transform;
-    		hit.GetComponent<ShipPieceScript>().Glue();
-    	}
+        savedVelocity = rb.velocity;
     }
 
+    public void OnTriggerEnter(Collider c) {
+    	hit = c.gameObject;
+        if (hit.CompareTag("shippiece") && hit.GetComponent<ShipPieceScript>().inPlace == false)
+        {
+            glued = true;
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
+            hit.GetComponent<Rigidbody>().isKinematic = true;
+            hit.GetComponent<Rigidbody>().transform.parent = GetComponent<Rigidbody>().transform;
+            hit.GetComponent<ShipPieceScript>().Glue();
+        }
+        StartCoroutine("ResetVelocity");
+    }
+
+    IEnumerator ResetVelocity()
+    {
+        yield return new WaitForSeconds(0.1f);
+        rb.velocity = savedVelocity;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("shippiece"))
+            savedVelocity = rb.velocity;
+    }
 }
