7e63e99d801b44213bbebcccdfb171f402bffa9b
SeamusMcFarland <60547736+SeamusMcFarland@users.noreply.github.com>
2020-02-02 15:01:50 -0600

    audio more

--- Assets/AudioManagerScript.cs
+++ Assets/AudioManagerScript.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class AudioManagerScript : MonoBehaviour
 {
+    public AudioClip glueHit;
+    public AudioClip grab;
+    public AudioClip hurt;
+    public AudioClip shipHit;
+    public AudioClip fittingTogether;
     public AudioClip shoot;
+    public AudioClip breaking;
     AudioSource audioSource;
 
     // Start is called before the first frame update
@@ -13,15 +19,35 @@ public class AudioManagerScript : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGlueHit()
     {
-        
+        audioSource.PlayOneShot(glueHit);
     }
 
+    public void PlayGrab()
+    {
+        audioSource.PlayOneShot(grab);
+    }
+    public void PlayHurt()
+    {
+        audioSource.PlayOneShot(hurt);
+    }
+    public void PlayShipHit()
+    {
+        audioSource.PlayOneShot(shipHit);
+    }
+    public void PlayFittingTogether()
+    {
+        audioSource.PlayOneShot(fittingTogether);
+    }
     public void PlayShoot()
     {
         audioSource.PlayOneShot(shoot);
+    }
+
+    public void PlayBreaking()
+    {
+        audioSource.PlayOneShot(breaking);
     }
 
 }
