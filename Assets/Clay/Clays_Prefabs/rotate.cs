7f17b12b283dcd2ddbdd1daf1cf56f57e64c85e9
clayrehling <clayrehling@hotmail.com>
2020-02-02 15:17:31 -0600

    Adding Clay's Assets - skybox and 2 junk prefabs
    Adding Clay's Assets - skybox and 2 junk prefabs

---
+++ Assets/Clay/Clays_Prefabs/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float speed =0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed);
+        transform.Rotate(Vector3.right, speed/3);
+    }
+}
